432114c5c99398387efb955daa2f683e6b3f1a14
contyadvait <advaitthepro@outlook.com>
2024-06-28 23:26:43 +0800

    Initial Commit

---
+++ inventory/InventoryObjects.swift
@@ -0,0 +1,31 @@
+//
+//  InventoryObjects.swift
+//  inventory
+//
+//  Created by Milind Contractor on 28/6/24.
+//
+
+import Foundation
+
+enum Kallax: Codable {
+    case shelf(number: Int)
+    case drawer(number: Int)
+}
+
+enum Alex: Codable {
+    case left(number: Int)
+    case right(number: Int)
+}
+
+enum Shelf: Codable {
+    case kallax(location: Kallax)
+    case alex(location: Alex)
+}
+
+struct Object: Identifiable, Codable {
+    let id = UUID()
+    
+    var name: String
+    var location: Shelf
+    var description: String
+}
